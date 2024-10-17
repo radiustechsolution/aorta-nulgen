94119ea93073ed4a1bcf45fade16c68c688b8a8a
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-17 07:16:55 +0100

    Updated

--- components/landing-page/TestimonialComp.tsx
+++ components/landing-page/TestimonialComp.tsx
@@ -14,7 +14,7 @@ export const TestimonialComp = () => {
             height={50}
           />
           <div className="">
-            <p className="text-[17px] sm:text-[20px] font-ProximaNova font-bold">
+            <p className="text-[17px] text-black sm:text-[20px] font-ProximaNova font-bold">
               140+
             </p>
             <p className="text-[12px] text-gray-500">Countries Served</p>
@@ -29,7 +29,7 @@ export const TestimonialComp = () => {
             height={50}
           />
           <div className="">
-            <p className="text-[17px] sm:text-[20px] font-ProximaNova font-bold">
+            <p className="text-[17px] text-black sm:text-[20px] font-ProximaNova font-bold">
               25,000+
             </p>
             <p className="text-[12px] text-gray-500">Clients Served</p>
