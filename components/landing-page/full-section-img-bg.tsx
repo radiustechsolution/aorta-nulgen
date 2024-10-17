c83b5ad0c0021a4c372f2701e66131c489acbe62
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-17 06:57:33 +0100

    Updated

--- components/landing-page/full-section-img-bg.tsx
+++ components/landing-page/full-section-img-bg.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const FullWidthSection = () => {
   return (
     <div
-      className="relative flex flex-col items-start px-[30px] sm:px-[100px]  md:px-[150px] lg:px-[200px] justify-center h-[440px] text-white bg-cover bg-center"
+      className="relative flex flex-col items-start px-[30px] sm:px-[100px]  md:px-[150px] lg:px-[250px] xl:px-[350px] justify-center h-[440px] text-white bg-cover bg-center"
       style={{
         backgroundImage: "url(/asset/full-img.jpeg)",
       }}
@@ -16,7 +16,7 @@ export const FullWidthSection = () => {
       {/* Quote and Logos */}
       <div className="relative z-10 text-left px-4 md:px-0">
         <RiDoubleQuotesL size={44} color="#204ECF" />
-        <p className="text-xl pt-5 md:text-3xl font-ProximaNova font-medium mb-4">
+        <p className="text-xl pt-5 md:text-3xl font-ProximaNova max-w-[1200px] font-medium mb-4">
           We’re known as a high-skilled marketplace, and we see an acute pain
           point within that area.
         </p>
