571935cc5ef46f0842e27b8e2477508cc138261e
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-22 06:32:45 +0100

    Addded components

--- components/onboard/navbar.tsx
+++ components/onboard/navbar.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <nav className="h-max md:h-[80px] w-full flex flex-col md:flex-row items-center">
       <div className="w-full md:w-[300px] px-7 flex items-center justify-start md:justify-end h-[60px] md:h-[80px] bg-[#0F256E] md:bg-gradient-to-b from-[#1F4DCD] to-[#173BA0]">
-        <h1 className="text-[30px] font-ProximaNova font-semibold text-white">
+        <Link
+          href={"/"}
+          className="text-[30px] font-ProximaNova font-semibold text-white"
+        >
           Nulgen
-        </h1>
+        </Link>
       </div>
       <div className="w-full md:w-[84%] px-7 h-[60px] md:h-[80px] flex items-center justify-between bg-[#08164E] md:bg-gradient-to-b from-[#0E246D] to-[#081750]">
         <div>
