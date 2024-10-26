8e4a69cfd6646b382130c899c839cd08aa53c249
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-26 06:43:03 +0100

    updated

--- pages/jobs/[id].tsx
+++ pages/jobs/[id].tsx
@@ -8,6 +8,7 @@ import { MdCategory, MdOutlineCategory, MdOutlineWork } from "react-icons/md";
 import { RiTimeFill, RiTimeLine } from "react-icons/ri";
 import { PiBagLight } from "react-icons/pi";
 import { IoIosArrowRoundBack } from "react-icons/io";
+import { IoNewspaperOutline } from "react-icons/io5";
 
 export default function BlogDetails() {
   const router = useRouter();
@@ -17,7 +18,7 @@ export default function BlogDetails() {
   if (!job) {
     return (
       <div>
-        <p>Not found</p>
+        <p></p>
       </div>
     );
   }
@@ -44,6 +45,7 @@ export default function BlogDetails() {
           </div>
         </div>
         <div className="max-w-[1100px] w-full flex justify-between items-start">
+          {/* Left */}
           <div className="rounded-lg border flex flex-col gap-5 border-gray-200 overflow-hidden w-full md:w-[70%] ">
             {/* Top */}
             <div className="h-[400px] sm:h-[290px] relative w-full">
@@ -138,12 +140,94 @@ export default function BlogDetails() {
                 deleniti ipsum, quas nulla facilis porro! Lorem ipsum dolor sit
                 amet consectetur, adipisicing elit. Rem dolor minima eum laborum
                 qui, porro dignissimos vero in incidunt error facere. Quis
-                magnam quasi deleniti ipsum, quas nulla facilis porro!
+                magnam quasi deleniti ipsum, quas nulla facilis porro! Lorem
+                ipsum dolor sit amet consectetur, adipisicing elit. Rem dolor
+                minima eum laborum qui, porro dignissimos vero in incidunt error
+                facere. Quis magnam quasi deleniti ipsum, quas nulla facilis
+                porro! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Rem dolor minima eum laborum qui, porro dignissimos vero in
+                incidunt error facere. Quis magnam quasi deleniti ipsum, quas
+                nulla facilis porro! Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Rem dolor minima eum laborum qui, porro
+                dignissimos vero in incidunt error facere. Quis magnam quasi
+                deleniti ipsum, quas nulla facilis porro! Lorem ipsum dolor sit
+                amet consectetur, adipisicing elit. Rem dolor minima eum laborum
+                qui, porro dignissimos vero in incidunt error facere. Quis
+                magnam quasi deleniti ipsum, quas nulla facilis porro! Lorem
+                ipsum dolor sit amet consectetur, adipisicing elit. Rem dolor
+                minima eum laborum qui, porro dignissimos vero in incidunt error
+                facere. Quis magnam quasi deleniti ipsum, quas nulla facilis
+                porro! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Rem dolor minima eum laborum qui, porro dignissimos vero in
+                incidunt error facere. Quis magnam quasi deleniti ipsum, quas
+                nulla facilis porro! Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Rem dolor minima eum laborum qui, porro
+                dignissimos vero in incidunt error facere. Quis magnam quasi
+                deleniti ipsum, quas nulla facilis porro! Lorem ipsum dolor sit
+                amet consectetur, adipisicing elit. Rem dolor minima eum laborum
+                qui, porro dignissimos vero in incidunt error facere. Quis
+                magnam quasi deleniti ipsum, quas nulla facilis porro! Lorem
+                ipsum dolor sit amet consectetur, adipisicing elit. Rem dolor
+                minima eum laborum qui, porro dignissimos vero in incidunt error
+                facere. Quis magnam quasi deleniti ipsum, quas nulla facilis
+                porro! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Rem dolor minima eum laborum qui, porro dignissimos vero in
+                incidunt error facere. Quis magnam quasi deleniti ipsum, quas
+                nulla facilis porro! Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Rem dolor minima eum laborum qui, porro
+                dignissimos vero in incidunt error facere. Quis magnam quasi
+                deleniti ipsum, quas nulla facilis porro!
               </p>
             </div>
           </div>
-          <div className="rounded-md sticky top-10 hidden md:block w-[27%] bg-red-300">
-            <p>Right</p>
+
+          {/* Right */}
+          <div className="sticky top-10 hidden flex-col gap-4 md:flex w-[27%]">
+            {/* First */}
+            <div className="rounded-lg px-4 py-5 border border-gray-200 bg-white">
+              {/* Top */}
+              <div className="flex flex-col gap-2">
+                <div className="h-[35px] w-[35px] rounded-md bg-[#136EF5] flex items-center justify-center">
+                  <BiLogoCPlusPlus color="white" size={25} />
+                </div>
+                <div>
+                  <p className="font-[700] leading-[30px] text-[17px] text-[#232535] ">
+                    About {job.company}
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-[16px] my-3 text-[#484b62] font-[400] leading-[26px]">
+                Excepteur sint occaecat siter dolor cupidatat non proider sunt
+                in culpa qui official ers.
+              </p>
+
+              <button className="bg-[#FBFBFC] rounded-md font-semibold border border-[#EAEBF4] w-full p-2">
+                <p className="text-black text-[14px]">View Company</p>
+              </button>
+            </div>
+            {/* Second */}
+            <div className="rounded-lg px-4 py-5 border border-gray-200 bg-white">
+              {/* Top */}
+              <div className="flex flex-col gap-2">
+                <div className="h-[35px] w-[35px] rounded-md border border-[#EAEBF4] bg-[#ffffff] flex items-center justify-center">
+                  <IoNewspaperOutline color="#136EF5" size={20} />
+                </div>
+                <div>
+                  <p className="font-[700] leading-[30px] text-[17px] text-[#232535] ">
+                    Post a job today
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-[16px] my-3 text-[#484b62] font-[400] leading-[26px]">
+                I understand that this feature will not be included.
+              </p>
+
+              <button className="bg-[#136EF5] rounded-md font-semibold border w-full p-2">
+                <p className="text-white text-[14px]">Post a featured job</p>
+              </button>
+            </div>
           </div>
         </div>
       </div>
